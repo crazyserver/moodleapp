54b8a6be21a73d953b71e0d57d339bdd4d93a7a6
Pau Ferrer Ocaña <crazyserver@gmail.com>
2026-09-04 10:02:02 +0200

    Add capacitor assets

---
+++ scripts/generate-assets.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# More info https://github.com/ionic-team/capacitor-assets
+npx @capacitor/assets generate --iconBackgroundColor '#ffffff' --iconBackgroundColorDark '#111111' --splashBackgroundColor '#ffffff' --splashBackgroundColorDark '#111111'
